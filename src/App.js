193471677901f0186309c07f12cdafcb7fdd74e0
Scott Ian <sim2487@gmail.com>
2020-04-16 21:50:19 -0700

    Add top level components. Add basic routing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-function App() {
+export const App = () => {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>Hello TaskMastr</p>
-			</header>
-		</div>
+		<Router>
+			<div>
+				<Route exact path='/' component={Dashboard} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/signup' component={Signup} />
+			</div>
+		</Router>
 	);
-}
-
-export default App;
+};
