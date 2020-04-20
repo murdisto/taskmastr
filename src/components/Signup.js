8919ec2b42d163aae1a726dd8b30843ab0975a50
Scott Ian <sim2487@gmail.com>
2020-04-19 18:52:43 -0700

    Signup component functionality and structure.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
+import { firebase } from '../firebase';
 
-export const Signup = () => {
+export const Signup = ({ history }) => {
+	// useCallback hook. returns memoized call back.
+	// every value referenced inside callback should also appear in the dependencies array
+	const handleSignup = useCallback(
+		async (event) => {
+			event.preventDefault(); // prevent page reload on submit
+			const { email, password } = event.target.elements; // grab the email and password inputs
+			try {
+				await firebase
+					.auth() // grab the input values and send them to firebase to create a new user
+					.createUserWithEmailAndPassword(email.value, password.value);
+				history.push('/'); // then redirect to that user's dashboard
+			} catch (error) {
+				alert(error); // show an error if there is one
+			}
+		},
+		[history]
+	);
+
 	return (
-		<div>
-			Signup
-			<Link to='/login'>Log in</Link>
+		<div className='signup'>
+			<h2>Sign Up!</h2>
+			<form onSubmit={handleSignup}>
+				<label htmlFor='email'>
+					<input name='email' type='email' placeholder='Email' />
+				</label>
+				<label htmlFor='email'>
+					<input name='password' type='password' placeholder='Password' />
+				</label>
+				<button type='submit'>Sign Up</button>
+			</form>
+			Already have an account? <Link to='/login'>Log in</Link>
 		</div>
 	);
 };
