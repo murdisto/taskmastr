113c6ffbd966c72251d1ee6793307953353452da
Scott Ian <sim2487@gmail.com>
2020-04-25 22:32:42 -0700

    Header styles.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,16 +5,24 @@ import Taskmastr from '../../images/taskmastr';
 export const Header = () => {
 	return (
 		<header className='header'>
-			header rendering!
 			<nav>
 				<div className='logo'>
 					<Taskmastr /> <p>TaskMastr</p>
 				</div>
 				<div className='settings'>
 					<ul>
-						<li>
+						<li className='settings__add'>
+							<button
+								onClick={() => {
+									console.log('settings__add clicked');
+								}}
+								type='button'
+							>
+								+
+							</button>
+						</li>
+						<li className='settings__darkmode'>
 							<button type='button'>
-								{/* for darkmode later */}
 								<BsMoon />
 							</button>
 						</li>
